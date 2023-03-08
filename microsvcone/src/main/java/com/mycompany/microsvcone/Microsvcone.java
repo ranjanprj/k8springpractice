36aef6adc117518e36e62bd5165f39a15ed0c093
Ranjan <pranjan24@dxc.com>
2023-03-08 16:17:42 +0530

    first commit

---
+++ microsvcone/src/main/java/com/mycompany/microsvcone/Microsvcone.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.microsvcone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author pranjan24
+ */
+@RestController
+@SpringBootApplication
+public class Microsvcone {
+    
+    @RequestMapping("/")
+    String home(){
+        return "hello world";
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Microsvcone.class,args);
+    }
+}
